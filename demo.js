d86c26a1f41fd0d1b856fdf43eeb9a0d690e9c5b
dnlsyfq <dan90@live.com.my>
2021-04-18 20:26:41 +0800

    update

--- demo.js
+++ demo.js
@@ -1,6 +1,75 @@
+//  enforce cleaner js 
 'use strict'; 
-(function() {
+// 
+// (function() {
 
-  display('Hello World');
+//   display('Hello World'); 
 
-})();
+// })();
+
+// create object literal 
+
+/*
+(
+  function(){
+    let person = {
+      firstName:'Jim',
+      lastName:'Cooper',
+      isAdult: function(){
+        return person.age >= 18;
+      }
+    };
+
+    person.age = 29;
+    person.isAdult = function(){return this.age >= 18; }
+
+    display(person.firstName);
+    display(person.age);
+
+    // document.getElementById('output').innerText = person.firstName;
+  }
+)();
+*/
+
+// create object literal 
+
+(
+  function(){
+
+    function registerUser(fName,lName){
+      let person = {
+        firstName: fName,
+        lastName: lName
+      }
+
+      display(person);
+    }
+
+    registerUser('Jim','Cooper');
+
+
+
+  }
+)();
+
+// create object literal
+(
+  function(){
+    let person = {
+      firstName:'Jim',
+      lastName:'Cooper',
+      age:17,
+      isAdult(){
+        return this.age >= 18;
+      }
+    };
+
+    display(person.isAdult());
+    display(Object.keys(person));
+    display(person);
+
+    for(let propertyName in person){
+      display(propertyName);
+    }
+  }
+)();
